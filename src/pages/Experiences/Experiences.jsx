2ad7fae7a0ec766f1aad95af4057d6b1a5f9782f
Felix Felix <felixharris11@gmail.com>
2025-06-02 13:40:43 +0700

    changing height in experience

--- src/pages/Experiences/Experiences.jsx
+++ src/pages/Experiences/Experiences.jsx
@@ -106,8 +106,8 @@ function Experiences({ experienceRef }) {
   }
   return (
     <section
-      className="snap-section"
-      style={{ height: "200vh", justifyContent: "flex-start" }}
+      className={`snap-section ${styles.experienceSection}`}
+      style={{ height: "150vh", justifyContent: "flex-start" }}
       ref={experienceRef}
     >
       <div className="flex flex-row items-start w-2/2 justify-between">
